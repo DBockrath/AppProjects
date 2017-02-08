97047042bd96e8fe17b3de7ec5b90dd02db02523
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-02-08 08:53:32 -0500

    Update Travel.java

--- GalaxyTraders/app/src/main/java/com/DTMSoundMaster/GalaxyTraders/Travel.java
+++ GalaxyTraders/app/src/main/java/com/DTMSoundMaster/GalaxyTraders/Travel.java
@@ -24,5 +24,68 @@ public class Travel {
 		
 		}
 	 }
+	 public void Venus() {
+	 
+	 Scanner scanner = new Scanner(System.in);
+		 
+		private boolean confirm;
+		private String input;
+		 
+		System.out.println("Are you sure you want to travel to Venus?");
+		System.out.println("Enter 'yes' or 'no'");
+		input = scanner.nextLine();
+		
+		if (input == "yes") {
+		
+			confirm = true;
+		
+		} else {
+		
+			confirm = false;
+		
+		}
+	 }
+	 public void Earth() {
+	 
+	 Scanner scanner = new Scanner(System.in);
+		 
+		private boolean confirm;
+		private String input;
+		 
+		System.out.println("Are you sure you want to travel to Earth?");
+		System.out.println("Enter 'yes' or 'no'");
+		input = scanner.nextLine();
+		
+		if (input == "yes") {
+		
+			confirm = true;
+		
+		} else {
+		
+			confirm = false;
+		
+		}
+	 }
+	 public void Mars() {
+	 
+	 Scanner scanner = new Scanner(System.in);
+		 
+		private boolean confirm;
+		private String input;
+		 
+		System.out.println("Are you sure you want to travel to Mars?");
+		System.out.println("Enter 'yes' or 'no'");
+		input = scanner.nextLine();
+		
+		if (input == "yes") {
+		
+			confirm = true;
+		
+		} else {
+		
+			confirm = false;
+		
+		}
+	 }
 	
 }
