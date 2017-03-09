04912cfc2913da8bdc94360b3edb011ac9711d4d
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-03-09 15:08:26 -0500

    added GetIPAddressSimple

---
+++ GetIPAddressSimple/src/Main.java
@@ -0,0 +1,27 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+
+class Main {
+	
+	public static void main(String[] args) throws UnknownHostException {
+		
+		try {
+			
+		InetAddress localhost = InetAddress.getLocalHost();
+		String ip = localhost.getHostAddress();
+		String hostname = localhost.getHostName();
+		// List addresses = InetAddress.getAllByName(localhost.getCanonicalHostName());
+		
+		System.out.println(ip);
+		System.out.println(hostname);
+		
+		} catch (UnknownHostException ex) {
+			
+			ex.printStackTrace();
+			
+		}
+		
+	}
+	
+}
