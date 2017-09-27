eda9c1abc38a772347e27a8a4d27a852d42906d2
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-09-27 09:34:51 -0400

    Updated the program

--- NewtonsGravityEquation/src/Main.java
+++ NewtonsGravityEquation/src/Main.java
@@ -1,20 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scaner scanner = new Scanner(System.in);
+		
+		int F;
+		int G = 6.674 * 10^(-11);
+		int m1;
+		int m2;
+		int r;
+		
+		System.out.println("Enter the force between masses in newtons: ");
+		F = scanner.nextInt();
+		System.out.println("Enter the mass of the first object in kilograms: ");
+		m1 = scanner.nextInt();
+		System.out.println("Enter the distance between the two objects in meters: ");
+		r = scanner.nextInt();
+		
+		Int m2 = ((F / G) * ((r)^2)) / (m1);
+		System.out.println("The mass of the second object is: " + m2);
+		
 	}
+	
 }
